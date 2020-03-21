414a40d309140ab6ff46ecc40f38fa99e6845432
hollygoaightly <62455705+hollygoaightly@users.noreply.github.com>
2020-03-21 10:06:02 -0700

    Update Hand.java

--- src/com/company/Hand.java
+++ src/com/company/Hand.java
@@ -1,4 +1,54 @@
 package com.company;
 
-public class Hand {
-}
+class Hand 
+{
+    // Limit size of array
+    public static final int MAX_CARDS = 100;    
+
+    private Card[] myCards;
+    private int numCards;
+
+    // Default constructor
+    public void Hand()
+    {
+        resetHand();
+    }
+    // Remove all cards from the hand
+    public void resetHand()
+    {
+        myCards = new int[MAX_CARDS];
+        numCards = 0;
+    }
+    // Add a card to the next available position in the myCards array
+    public boolean takeCard(Card card)
+    {
+        myCards[numCards] = card;
+        numCards += 1;
+    }
+    // Removes and returns the card in the top occupied position of the array
+    public Card playCard()
+    {
+        Card card = myCards[numCards];
+        myCards[numCards] = null;
+        numCards -= 1;
+        return card;
+    }
+    // Diplay the entire hand
+    public String toString()
+    {
+        String result = '';
+        for(int i = 0; i< numCards; i++)
+        {
+
+        }
+    }
+    // Diplay the entire hand
+    public int getNumCards()
+    {
+
+    }
+    // Access individual card. Return card with error flag if k is bad
+    public Card inspectCard(int k)
+    {
+
+    }
