308fb18b513dd70e311eb619017a893e9cc59769
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-21 11:01:29 -0700

    Document Card class

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,7 +1,10 @@
 package com.company;
 
+/**
+ * Card class represents a single card.
+ */
 public class Card {
-   public static enum Suit {
+   public static enum Suit { // All possible suits of a card
       CLUBS,
       DIAMONDS,
       HEARTS,
@@ -9,33 +12,64 @@ public class Card {
    };
    private char value;
    private Suit suit;
-   private boolean errorFlag;
+   private boolean errorFlag; // Whether this Card has an error.
 
-   public Card() {
+   /**
+    * No argument constructor
+    */
+   Card() {
       value = 'A';
       suit = Suit.SPADES;
    }
 
-   public Card(char value, Suit suit) {
+   /**
+    * Constructor with value and suit.
+    * @param value The value of the card.
+    * @param suit  The suit of the card.
+    */
+   Card(char value, Suit suit) {
       set(value, suit);
    }
 
+   /**
+    * Check if the given Card object equals this one.
+    * @param card Card instance to check for equality to this.
+    * @return     Whether the Card objects are equal.
+    */
    public boolean equals(Card card) {
       return card.getSuit() == suit && card.getValue() == value;
    }
 
+   /**
+    * Check if this Card has an error.
+    * @return Whether the Card object has an error.
+    */
    public boolean getErrorFlag() {
       return errorFlag;
    }
 
+   /**
+    * Get the suit of this card.
+    * @return Suit of card.
+    */
    public Suit getSuit() {
       return suit;
    }
 
+   /**
+    * Get the value of this card.
+    * @return Value of this card.
+    */
    public char getValue() {
       return value;
    }
 
+   /**
+    * Check if the parameters given are valid for a Card.
+    * @param value Value given.
+    * @param suit  Suit given.
+    * @return      Whether the parameters are valid.
+    */
    private boolean isValid(char value, Suit suit) {
       if (value >= '2' && value <= '9') {
          return true;
@@ -53,6 +87,12 @@ public class Card {
       }
    }
 
+   /**
+    * Set given parameters for Card.
+    * @param value Value to set for Card.
+    * @param suit  Suit to set for Card.
+    * @return      Whether the parameters were able to be set.
+    */
    public boolean set(char value, Suit suit) {
       if (isValid(value, suit)) {
          errorFlag = false;
@@ -65,6 +105,10 @@ public class Card {
       return !errorFlag;
    }
 
+   /**
+    * Display Card as a String.
+    * @return Card data as a String.
+    */
    public String toString() {
       if (!errorFlag) {
          return value + " of " + suit;
