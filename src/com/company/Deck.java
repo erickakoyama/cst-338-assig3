eb7fddf9db0947a74acc074d47fd5fc5f43fe80d
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-21 13:18:42 -0700

    Add more stub info to Deck

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,29 +1,49 @@
 package com.company;
 
 class Deck {
-   public final int MAX_CARDS = 312;
-   private static Card[] masterClass = new Card[52];
+   public final int MAX_CARDS = 6*52; // 6 packs of 52 cards
+   private static Card[] masterPack = new Card[52];
    private Card[] cards;
    private int topCard;
-   public Deck(int numPacks) {
+
+   Deck() {
+      // no argument constructor, assume 1 pack
    }
+
+   Deck(int numPacks) {
+      // populate various arrays
+   }
+
    public void init(int numPacks) {
    }
+
    public void shuffle() {
+      // shuffle cards into random order
    }
+
    public Card dealCard() {
-      if () {
-      }
-      else
+      // have to remove a card from cards
+      return new Card();
    }
+
    public int getTopCard() {
       return topCard;
    }
+
    public Card inspectCard(int k) {
-      if () {
-      }
-      else
+      // We need to return a Card with errorFlag = true, if k is out of range.
+      return new Card();
    }
+
+   /**
+    * Should not be called more than once, so even if there are multiple
+    * Deck instances in a client, this should only execute once.
+    */
    private static void allocateMasterPack(){
+      if (masterPack[0] != null) { // if there are already cards in masterPack we can return early
+         return;
+      }
+
+      // allocate master pack
    }
 }
