b0616fe4dadd9b96005ca3434c8bfc78c5ee78e8
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-21 11:32:10 -0700

    Copy in Deck class scaffold from slack

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,4 +1,29 @@
 package com.company;
 
-public class Deck {
+class Deck {
+   public final int MAX_CARDS = 312;
+   private static Card[] masterClass = new Card[52];
+   private Card[] cards;
+   private int topCard;
+   public Deck(int numPacks) {
+   }
+   public void init(int numPacks) {
+   }
+   public void shuffle() {
+   }
+   public Card dealCard() {
+      if () {
+      }
+      else
+   }
+   public int getTopCard() {
+      return topCard;
+   }
+   public Card inspectCard(int k) {
+      if () {
+      }
+      else
+   }
+   private static void allocateMasterPack(){
+   }
 }
