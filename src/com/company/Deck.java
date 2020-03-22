50ca25b20952a809f777352cfe36b9f5db9e21c3
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-22 11:00:53 -0700

    Fill in some Deck class methods

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,38 +1,69 @@
 package com.company;
 
+import com.company.Card.Suit;
+
 public class Deck {
-   public final int MAX_CARDS = 6*52; // 6 packs of 52 cards
-   private static Card[] masterPack = new Card[52];
-   private Card[] cards;
+   public final int MAX_CARDS = 312; // 6*52 packs of 52 cards
+   private static int PACK_SIZE = 52;
+   private static Card[] masterPack = new Card[PACK_SIZE];
+   private Card[] cards = new Card[MAX_CARDS];
    private int topCard;
 
+   /**
+    * No argument constructor
+    */
    Deck() {
-      // no argument constructor, assume 1 pack
+      this(1); // default size of 1 pack
+
+      allocateMasterPack();
    }
 
+
    Deck(int numPacks) {
-      // populate various arrays
+      topCard = 0;
+      allocateMasterPack();
+
+      init(numPacks);
    }
 
    public void init(int numPacks) {
+      // fill up cards array
+      for (int i = 0; i < numPacks; i++) {
+         for (int j = 0; j < PACK_SIZE; j++) {
+            topCard++;
+            cards[(PACK_SIZE * i) + j] = new Card(masterPack[j]);
+         }
+      }
    }
 
    public void shuffle() {
-      // shuffle cards into random order
+      // mixes up the cards with the help of the standard random number generator.
    }
 
    public Card dealCard() {
-      // have to remove a card from cards
-      return new Card();
+      if (topCard > 0) {
+         topCard--;
+         return new Card(cards[topCard]);
+      } else {
+         return new Card('X', Suit.SPADES);
+      }
    }
 
    public int getTopCard() {
       return topCard;
    }
 
+   /**
+    * Return a Card with errorFlag = true, if k is out of range.
+    * @param k
+    * @return
+    */
    public Card inspectCard(int k) {
-      // We need to return a Card with errorFlag = true, if k is out of range.
-      return new Card();
+      if (k <= topCard) {
+         return new Card(cards[k]);
+      }
+
+      return new Card('X', Suit.DIAMONDS);
    }
 
    /**
@@ -44,6 +75,14 @@ public class Deck {
          return;
       }
 
-      // allocate master pack
+      Suit[] suits = Suit.values();
+      char[] values = {'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'A'};
+
+      // Load all the cards into the master pack
+      for (int i = 0; i < values.length; i++) {
+         for (int j = 0; j < suits.length; j++) {
+            masterPack[(suits.length * i) + j] = new Card(values[i], suits[j]);
+         }
+      }
    }
 }
