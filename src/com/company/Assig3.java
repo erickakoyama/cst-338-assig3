5866aa507bc355224cbef447255aa46422818344
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-21 18:25:59 -0700

    Stub out outputs for Phase 4 testing of Deck and Hand

--- src/com/company/Assig3.java
+++ src/com/company/Assig3.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.util.*;
+
 public class Assig3 {
 
     public static void main(String[] args) {
-      testCard();
+       Scanner keyboard = new Scanner(System.in);
+
+       testCard(); // TODO: Delete this before turning in: Only Phase 3 and 4 output are required!
+
+       System.out.println();
+
+       assign3Tester(keyboard);
+
+       keyboard.close();
     }
 
     public static void testCard() {
@@ -23,4 +33,60 @@ public class Assig3 {
        System.out.println(cardTwo.toString());
        System.out.println(cardThree.toString());
     }
+
+   /**
+    * Test Deck and Hand class
+    */
+   public static void assign3Tester(Scanner keyboard) {
+      int numPlayers;
+      int minPlayers = 1;
+      int maxPlayers = 10;
+      Hand[] players = new Hand[maxPlayers];
+
+
+      do {
+         System.out.print("How many hands? (" + minPlayers + " - " + maxPlayers + ", please): ");
+         numPlayers = keyboard.nextInt();
+
+         for (int i = 0; i < numPlayers; i++) {
+            players[i] = new Hand();
+         }
+
+         Deck deck = new Deck();
+         dealOutDeck(deck, players);
+
+         System.out.println("Here are our hands, from unshuffled deck:");
+         printHands(players);
+
+         // reset all hands
+         for(Hand hand: players) {
+            hand.reset();
+         }
+
+         // shuffle deck and deal again
+         deck.shuffle();
+         dealOutDeck(deck, players);
+
+         System.out.println("Here are our hands, from SHUFFLED deck:");
+         printHands(players);
+
+      } while (numPlayers < minPlayers || numPlayers > maxPlayers); // keep asking until we get a valid input.
+   }
+
+   public static void dealOutDeck(Deck deck, Hand[] players) {
+      // commenting this out until we have more code for Deck
+//      while (deck.getTopCard() >= 0) {
+//         for(Hand player: players) {
+//            if (deck.getTopCard() >= 0) {
+//               player.takeCard(deck.dealCard());
+//            }
+//         }
+//      }
+   }
+
+   public static void printHands(Hand[] hands) {
+      for (Hand hand: hands) {
+         System.out.println(hand.toString());
+      }
+   }
 }
--- src/com/company/Hand.java
+++ src/com/company/Hand.java
@@ -1,4 +1,7 @@
 package com.company;
 
 public class Hand {
+   public void reset() {} // mocked just for test
+
+   public String toString() { return "mockHand";} // mocked just for test
 }
